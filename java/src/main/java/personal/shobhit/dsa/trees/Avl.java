3d6b43db72c130d6c68a562607321db96549ad9c
Shobhit0109 <shobhit.ug21@nsut.ac.in>
2026-02-12 13:32:47 +0530

    Removed .idea folder

--- java/src/main/java/personal/shobhit/dsa/trees/Avl.java
+++ java/src/main/java/personal/shobhit/dsa/trees/Avl.java
@@ -3,11 +3,12 @@ package personal.shobhit.dsa.trees;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
+import lombok.NonNull;
 
 /**
- * Adelson-Velsky and Landis Tree (AVL) implementation with basic operations such as insertion,
- * deletion, finding, and more. This implementation allows duplicate values by inserting them to the
- * left subtree.
+ * Binary Search Tree (BST) implementation with basic operations such as insertion, deletion,
+ * finding, and more. This implementation allows duplicate values by inserting them to the left
+ * subtree.
  */
 @SuppressWarnings({"SameParameterValue", "ReturnOfNull", "java:S106"})
 @Builder
@@ -15,25 +16,31 @@ class Avl {
   private TreeNode node;
 
   static void main() {
-    TreeNode tree = null;
+    final var tree = TreeNode.builder().value(5).build();
+    final var tree2 = TreeNode.builder().value(0).build();
 
-    tree = insertAll(tree, 5, 3, 7, 2, 4, 6, 5, 1);
-    tree = merge(tree, insertAll(null, 8, 9));
+    insertAll(tree, 3, 7, 2, 4, 6, 5);
+    insertAll(tree2, 1, 2, 8, 9);
 
-    tree = deleteAll(tree, 7);
+    merge(tree, tree2);
 
-    final var popOutput = pop(tree);
-    System.out.println("pop output:" + popOutput.value());
-    tree = popOutput.node();
+    System.out.println("Original tree:");
+    TreeNode.print(tree);
+
+    deleteAll(tree, 7, 2);
+
+    System.out.println("pop output:" + pop(tree).value());
 
     System.out.println("\nfind 3: " + find(tree, 3));
 
-    tree = replace(tree, 4, 10);
+    replace(tree, 4, 10);
 
     System.out.println("\nlca of 6 and 10: " + lca(tree, 6, 10));
     System.out.println("lca of 5 and 10: " + lca(tree, 5, 10));
 
     System.out.println("\n4th smallest: " + findKthSmallest(tree, 4));
+
+    System.out.println("\nSorted List: " + bstToSortedList(tree));
 
     TreeNode.print(tree);
   }
@@ -43,105 +50,88 @@ class Avl {
     return (k > 0 && k <= sortedSet.size()) ? sortedSet.get(k - 1) : null;
   }
 
-  private static TreeNode lca(final TreeNode node, final Integer value1, final Integer value2) {
-    if (node == null) {
-      return null;
-    }
-
-    if (value1 <= node.getValue() && value2 <= node.getValue()) {
-      return lca(node.getLeft(), value1, value2);
-    } else if (value1 > node.getValue() && value2 > node.getValue()) {
-      return lca(node.getRight(), value1, value2);
-    }
-
-    return node;
+  private static TreeNode lca(final TreeNode node, final int value1, final int value2) {
+    return switch (node) {
+      case null -> null;
+      case final TreeNode n when value1 <= n.getValue() && value2 <= n.getValue() ->
+          lca(n.getLeft(), value1, value2);
+      case final TreeNode n when value1 > n.getValue() && value2 > n.getValue() ->
+          lca(n.getRight(), value1, value2);
+      default -> node;
+    };
   }
 
-  private static TreeNode replace(
-      final TreeNode node, final Integer oldValue, final Integer newValue) {
-    return insert(delete(node, oldValue), newValue);
+  private static void replace(
+      @NonNull final TreeNode root, final int oldValue, final int newValue) {
+    delete(root, oldValue);
+    insert(root, TreeNode.builder().value(newValue).build());
   }
 
-  private static TreeNode find(final TreeNode node, final Integer value) {
-    if (node == null) {
-      return null;
-    } else if (node.getValue().equals(value)) {
-      return node;
-    } else if (value < node.getValue()) {
-      return find(node.getLeft(), value);
-    } else {
-      return find(node.getRight(), value);
-    }
+  private static TreeNode find(final TreeNode node, final int value) {
+    return switch (node) {
+      case null -> null;
+      case final TreeNode n when n.getValue() == value -> n;
+      case final TreeNode n ->
+          value < n.getValue() ? find(n.getLeft(), value) : find(n.getRight(), value);
+    };
   }
 
   private static PopOutput pop(final TreeNode node) {
-    if (node == null) {
-      return PopOutput.builder().value(null).node(null).build();
-    }
-
-    if (node.getLeft() == null) {
-      return PopOutput.builder().value(node.getValue()).node(node.getRight()).build();
-    }
-
-    final var popOutput = pop(node.getLeft());
-    node.setLeft(popOutput.node());
-
-    return PopOutput.builder().value(popOutput.value()).node(node).build();
+    return switch (node) {
+      case null -> PopOutput.builder().value(null).node(null).build();
+      case final TreeNode n when n.getLeft() == null ->
+          PopOutput.builder().value(n.getValue()).node(n.getRight()).build();
+      default -> {
+        final var popOutput = pop(node.getLeft());
+        node.setLeft(popOutput.node());
+        yield PopOutput.builder().value(popOutput.value()).node(node).build();
+      }
+    };
   }
 
-  private static TreeNode insertAll(final TreeNode node, final Integer... values) {
-    TreeNode current = node;
+  private static void insertAll(@NonNull final TreeNode node, final Integer... values) {
     for (final var value : values) {
-      current = insert(current, value);
+      insert(node, TreeNode.builder().value(value).build());
     }
-    return current;
   }
 
-  private static TreeNode deleteAll(final TreeNode node, final Integer... values) {
-    TreeNode current = node;
+  private static void deleteAll(@NonNull final TreeNode node, final Integer... values) {
     for (final var value : values) {
-      current = delete(current, value);
+      delete(node, value);
     }
-    return current;
   }
 
-  private static TreeNode delete(final TreeNode node, final Integer value) {
-    if (node == null) {
-      return null;
-    }
-    var result = node.shallowCopy();
-    if (value < node.getValue()) {
-      result.setLeft(delete(node.getLeft(), value));
-    } else if (value > node.getValue()) {
-      result.setRight(delete(node.getRight(), value));
-    } else {
-      result = merge(node.getLeft(), node.getRight());
-    }
-    return result;
+  private static TreeNode delete(final TreeNode node, final int value) {
+    return switch (node) {
+      case null -> null;
+      case final TreeNode n when value < n.getValue() -> {
+        n.setLeft(delete(n.getLeft(), value));
+        yield n;
+      }
+      case final TreeNode n when value > n.getValue() -> {
+        n.setRight(delete(n.getRight(), value));
+        yield n;
+      }
+      case final TreeNode n -> {
+        if (n.getLeft() == null || n.getRight() == null) {
+          yield n.getRight() != null ? n.getRight() : n.getLeft();
+        } else {
+          yield TreeNode.builder()
+              .value(pop(n.getLeft()).value())
+              .left(n.getLeft())
+              .right(n.getRight())
+              .build();
+        }
+      }
+    };
   }
 
-  private static TreeNode merge(final TreeNode node1, final TreeNode node2) {
-    if (node1 == null) return node2;
-    if (node2 == null) return node1;
-
-    final var sortedList1 = bstToSortedList(node1);
-    final var sortedList2 = bstToSortedList(node2);
-
-    return mergeSortedListsToBst(sortedList1, sortedList2);
-  }
-
-  @SafeVarargs
-  private static TreeNode mergeSortedListsToBst(final List<Integer>... lists) {
-    // todo: merge sorted lists in O(n) time instead of O(n log n) time
-    final List<Integer> mergedList = new ArrayList<>();
-    for (final var list : lists) {
-      mergedList.addAll(list);
-    }
-    return insertAll(null, mergedList.toArray(Integer[]::new));
+  private static void merge(@NonNull final TreeNode node1, @NonNull final TreeNode node2) {
+    insert(node1, node2);
   }
 
   private static List<Integer> bstToSortedList(final TreeNode node) {
-    return bstToSortedList(node, new ArrayList<>());
+    return node == null ? new ArrayList<>() : bstToSortedList(node, new ArrayList<>());
   }
 
   private static List<Integer> bstToSortedList(final TreeNode node, final List<Integer> result) {
@@ -153,20 +143,18 @@ class Avl {
     return result;
   }
 
-  private static TreeNode insert(final TreeNode node, final Integer value) {
-    if (node == null) {
-      return TreeNode.builder().value(value).build();
-    }
-    final var result = node.shallowCopy();
-    if (node.getValue().equals(value)) {
-      final var newNode = TreeNode.builder().value(value).left(node.getLeft()).build();
-      result.setLeft(newNode);
-    } else if (value < node.getValue()) {
-      result.setLeft(insert(node.getLeft(), value));
-    } else {
-      result.setRight(insert(node.getRight(), value));
-    }
-    return result;
+  private static TreeNode insert(final TreeNode node, @NonNull final TreeNode value) {
+    return switch (node) {
+      case null -> value;
+      case final TreeNode n when value.getValue() <= n.getValue() -> {
+        n.setLeft(insert(n.getLeft(), value));
+        yield n;
+      }
+      case final TreeNode n -> {
+        n.setRight(insert(n.getRight(), value));
+        yield n;
+      }
+    };
   }
 
   @Builder
